f18201cee4348d2537bc1542f131013039e805bf
Michael50234 <michael.shi7m@gmail.com>
2026-02-19 23:49:17 -0500

    Performed migrations for quiz models

--- quiz_app/settings.py
+++ quiz_app/settings.py
@@ -48,8 +48,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt.token_blacklist",
     "accounts",
-    "quizzes"
-
+    "quizzes",
 ]
 
 MIDDLEWARE = [
--- quizzes/admin.py
+++ quizzes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Tag, Quiz, Question, Choice, Submission
 # Register your models here.
+
+admin.site.register(Quiz)
+admin.site.register(Question)
+admin.site.register(Choice)
+admin.site.register(Tag)
+admin.site.register(Submission)
