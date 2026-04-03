b68755249f2b602357b6ff797084f78b7fb3d09d
Michael50234 <michael.shi7m@gmail.com>
2026-04-03 11:47:39 -0400

    changed config in settings.py for deployment

--- quiz_app/settings.py
+++ quiz_app/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from datetime import timedelta
 from dotenv import load_dotenv
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,13 +27,12 @@ AUTH_USER_MODEL = 'accounts.User'
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fk)=@%(+&q*poqja)%y+4=xs4v)twa2rj6)(^4x$_j%l=9v2*)'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
 
 # Application definition
 
@@ -86,14 +86,9 @@ WSGI_APPLICATION = 'quiz_app.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE'  : 'django.db.backends.mysql', 
-        'NAME'    : os.getenv("DB_NAME"),               
-        'USER'    : os.getenv("DB_USER"),                     
-        'PASSWORD': os.getenv("DB_PASSWORD"),             
-        'HOST'    : os.getenv("DB_HOST"),                
-        'PORT'    : os.getenv("DB_PORT"),
-    }
+    'default': dj_database_url.parse(
+        os.getenv("MYSQL_URL")
+    )
 }
 
 
