98f37614d8fa682811485fcd97b0206b6c201f89
Michael50234 <michael.shi7m@gmail.com>
2026-04-03 12:45:33 -0400

    made changes to firebase config for deployment

--- quiz_app/firebase.py
+++ quiz_app/firebase.py
@@ -1,8 +1,10 @@
 import firebase_admin
 from firebase_admin import credentials
 import os
+import json
 from django.conf import settings
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate(os.path.join(settings.BASE_DIR, "firebase-admin-sdk.json"))
+    cred_dict = json.loads(os.getenv("CRED_DICT"))
+    cred = credentials.Certificate(cred_dict)
     firebase_admin.initialize_app(cred)
